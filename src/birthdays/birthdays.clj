78cc9a72c764ac3c2df55f8a080eb07109a63023
Blair Nangle <hi@blairnangle.com>
2023-10-25 09:04:12 +0100

    Fix loading of environment variables
    - Add basic logging
    - Wrap sending of emails in try-catch to stop program from aborting on one erroneous occasion
    - Stop printing of Omniconf values to stdout

--- src/birthdays/birthdays.clj
+++ src/birthdays/birthdays.clj
@@ -1,9 +1,13 @@
 (ns birthdays.birthdays
   (:require [clojure.data.json :as json]
             [postal.core :as postal]
-            [omniconf.core :as cfg]
+            [birthdays.config :as config]
             [clj-time.core :as time]
-            [clojure.walk :as walk]))
+            [clojure.walk :as walk]
+            [clojure.tools.logging :as logging]
+            [clojure.string :as string]))
+
+(def n-sent (atom 0))
 
 (defn- subject
   [occasion]
@@ -17,23 +21,27 @@
                    (= occasion :birthday) (str "Happy birthday, " name "!")
                    (= occasion :anniversary) (str "Happy anniversary, " name "!"))]
 
-    (str greeting "\n\n" (cfg/get :footer))))
+    (str greeting "\n\n" (config/get-config :footer))))
 
 (defn- send!
   [{:keys [emails name occasion]}]
-  (let [sender-email (cfg/get :sender-email)
-        gmail-password (cfg/get :gmail-password)]
-    (postal/send-message
-      {:host "smtp.gmail.com"
-       :user sender-email
-       :pass gmail-password
-       :port 587
-       :tls  true}
-      {:from    sender-email
-       :to      emails
-       :cc      (if (cfg/get :cc-sender) sender-email nil)
-       :subject (subject (keyword occasion))
-       :body    (body (keyword occasion) name)})))
+  (let [sender-email (config/get-config :sender-email)
+        gmail-password (config/get-config :gmail-password)]
+    (try
+      (postal/send-message
+        {:host "smtp.gmail.com"
+         :user sender-email
+         :pass gmail-password
+         :port 587
+         :tls  true}
+        {:from    sender-email
+         :to      emails
+         :cc      (if (config/get-config :cc-sender) sender-email nil)
+         :subject (subject (keyword occasion))
+         :body    (body (keyword occasion) name)})
+      (swap! n-sent inc)
+      (catch Exception e (str "Email to " (string/join ", " emails) " was not sent due to exception " (.getMessage e))
+                         (logging/error (str e))))))
 
 (defn- happening-today
   [occasions]
@@ -41,8 +49,8 @@
 
 (defn -main
   [& args]
-  (let [occasions (json/read-str (slurp (cfg/get :input-file)))
+  (config/load-config)
+  (let [occasions (json/read-str (slurp (config/get-config :input-file)))
         today (happening-today occasions)]
-    (cfg/populate-from-env)
-    (cfg/verify)
-    (run! send! (walk/keywordize-keys today))))
+    (run! send! (walk/keywordize-keys today))
+    (logging/info (str @n-sent (if (= @n-sent 1) " email" " emails") " sent!"))))
